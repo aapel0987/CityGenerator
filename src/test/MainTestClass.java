3b11399bceb63bcbead520ea6307cc53fa5a263f
aapel0987 <aapel0987@gmail.com>
2016-01-16 23:07:39 -0600

    Added the Java project, and some example code from the internet.

---
+++ src/test/MainTestClass.java
@@ -0,0 +1,10 @@
+package test;
+
+public class MainTestClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello again Java, my dear old friend!");
+	}
+
+}
